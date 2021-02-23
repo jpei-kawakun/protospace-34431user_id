a10719683d2b62ba180c297d9623c08033623072
jpei-kawakun <78395764+jpei-kawakun@users.noreply.github.com>
2021-02-23 15:40:26 +0900

    idが一致しないユーザーは編集できない

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,6 +26,12 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    if current_user.id == @prototype.user_id
+     redirect_to edit_prototype
+    else 
+      redirect_to root_path
+    end
+
   end
 
   def update
